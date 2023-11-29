54bcffc75b07c2fd0c9c58ef6b12493ddbb5a373
Richard MARTIN <richard.martin@polydev.app>
2023-11-29 01:25:38 -0500

    removing old code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Hero from "./Components/Hero";
 import Skills from "./Components/Skills";
-import {useEffect, useRef} from "react";
 import Experiences from "./Components/Experiences";
 import Education from "./Components/Education";
 import Projects from "./Components/Projects";
@@ -8,36 +7,6 @@ import Navbar from "./Components/Navbar";
 
 
 function App() {
-  const iconSize = "2x"
-
-  // const myBlob : HTMLElement = document.createElement("div");
-  // myBlob.style.overflow = "hidden";
-  // const myBlur : HTMLElement = document.createElement("div");
-  // myBlob.style.overflow = "hidden";
-  //
-  // useEffect(() => {
-  //   myBlob.id = "blob";
-  //   document.getElementById("app")!.appendChild(myBlob);
-  //   // myBlur.id = "blur";
-  //   // document.body.appendChild(myBlur);
-  //
-  // }, [])
-  //
-  // window.onpointermove = event => {
-  //   const pageX = event.pageX;
-  //   const pageY = event.pageY;
-  //
-  //   myBlob?.animate({
-  //     left: `${ pageX}px`,
-  //     top: `${ pageY}px`
-  //   }, { duration: 3000, fill: "forwards" });
-  //
-  //   // myBlur?.animate({
-  //   //   left: `${ pageX}px`,
-  //   //   top: `${ pageY}px`
-  //   // }, { duration: 3000, fill: "forwards" });
-  //
-  // }
 
   return (
     <div id="app" className="App">
