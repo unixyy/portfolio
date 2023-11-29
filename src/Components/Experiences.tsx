898bf1cc30bc33b71053629bbc588da2cdfc9cb5
Richard MARTIN <richard.martin@polydev.app>
2023-11-29 01:51:33 -0500

    Refactor grid layout in Experiences component
    Removed the 'grid' prop from BentoGrid component and simplified its implementation to use grid-cols. Also adjusted the column and row spans in Bento component for better arrangement. The changes lead to a more consistent and maintainable grid structure.

--- src/Components/Experiences.tsx
+++ src/Components/Experiences.tsx
@@ -1,12 +1,10 @@
 interface PropsBentoGrid {
-  grid : number;
   content: JSX.Element[];
 }
 
 function BentoGrid(props: PropsBentoGrid) {
-  const grid = "grid-rows-"+props.grid;
   return (
-    <div className={"grid "+grid+" auto-rows-[76px] gap-4"}>
+    <div className={"grid grid-cols-12 auto-rows-[76px] gap-4"}>
       {props.content.map((item, index) => (
         <>{item}</>
       ))}
@@ -16,15 +14,10 @@ function BentoGrid(props: PropsBentoGrid) {
 
 
 const Bento : JSX.Element[] = [
-  <div className={"rounded-md bg-emerald-800 col-span-3 row-span-3 "}>eh oh</div>,
-  <div className={"rounded-md bg-blue-500 col-span-1 row-span-3 "}>là ça va bon</div>,
-  <div className={"rounded-md bg-emerald-800 col-span-1 row-span-1 "}>eh oh</div>,
-  <div className={"rounded-md bg-emerald-800 col-span-1 row-span-1 "}>eh oh</div>,
-  <div className={"rounded-md bg-emerald-800 col-span-1 row-span-1 "}>eh oh</div>,
-  <div className={"rounded-md bg-emerald-800 col-span-1 row-span-1 "}>eh oh</div>,
-  <div className={"rounded-md bg-emerald-800 col-span-1 row-span-1 "}>eh oh</div>,
-  <div className={"rounded-md bg-emerald-800 col-span-1 row-span-1 "}>eh oh</div>,
-  <div className={"rounded-md bg-emerald-800 col-span-1 row-span-1 "}>eh oh</div>,
+  <div className={"rounded-md bg-emerald-800 col-span-2 row-span-4 "}>eh oh</div>,
+  <div className={"rounded-md bg-emerald-800 col-span-8 row-span-4 "}>eh oh</div>,
+  <div className={"rounded-md bg-emerald-800 col-span-2 row-span-2 "}>eh oh</div>,
+  <div className={"rounded-md bg-blue-500 col-span-2 row-span-2 "}>là ça va bon</div>,
 ]
 
 export default function Experiences() {
@@ -33,7 +26,7 @@ export default function Experiences() {
       <div className={"my-4 lg:my-16"}>
         <h1 className={"title gradExperience grad text-4xl"}>Mes Expériences</h1>
       </div>
-      <BentoGrid grid={4} content={Bento} />
+      <BentoGrid content={Bento} />
 
     </div>
   )
